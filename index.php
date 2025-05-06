f62407a787d7ef05afaeea77379c5fcd13506dab
Zhila <ministrov2018@gmail.com>
2025-05-06 14:00:26 +0300

    feature - Show or hide block to if else statement

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Выводит вконсоль браузера то значение, что вы передали ф-ции
  * @param mixed $data
  * @return $data
-*/
+ */
 
 function console_log($data)
 {
@@ -87,7 +87,7 @@ console_log($show_complete_tasks);
 
                         <label class="checkbox">
                             <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                            <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                            <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks > 0): ?> checked <?php endif; ?>>
                             <span class="checkbox__text">Показывать выполненные</span>
                         </label>
                     </div>
@@ -108,6 +108,20 @@ console_log($show_complete_tasks);
                             <td class="task__date"></td>
                         </tr>
                         <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                        <?php if ($show_complete_tasks > 0): ?>
+                            <tr class="tasks__item task task--completed">
+                                <td class="task__select">
+                                    <label class="checkbox task__checkbox">
+                                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                        <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                    </label>
+                                </td>
+                                <td class="task__date">10.10.2019</td>
+
+                                <td class="task__controls">
+                                </td>
+                            </tr>
+                        <?php endif; ?>
                     </table>
                 </main>
             </div>
