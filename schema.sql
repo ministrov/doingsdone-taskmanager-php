4b15e3aa9433fdff9a21c86b04c8fc0a862b68a1
Zhila <ministrov2018@gmail.com>
2025-05-12 13:10:17 +0300

    feature - Create a first table of project entity

--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE doings_done;
 USE doings_done;
 
-CREATE TABLE project ();
+CREATE TABLE project (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(128)
+);
 
 CREATE TABLE task ();
 
