0445c5eca59af99da0916eb2ec6faa73dbb1b5e6
Zhila <ministrov2018@gmail.com>
2025-05-13 10:48:05 +0300

    feature - Create a new file queries sql for inserting data to tables of data base MySQL

---
+++ queries.sql
@@ -0,0 +1,7 @@
+/*
+ Добавление новых записей в таблицу users
+*/
+
+INSERT INTO users (email, name, password)
+VALUES ("vasilisa@yandex.ru", "Василиса", "rnjnfv"),
+        ("nikifor@gmail.com", "Никифор", "zpltcm");
