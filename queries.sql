2723354348477ca08d2b3119cbfc782694cf0159
Zhila <ministrov2018@gmail.com>
2025-05-13 11:49:28 +0300

    feature - Add a new records into projects table of MySQL database

--- queries.sql
+++ queries.sql
@@ -5,3 +5,20 @@
 INSERT INTO users (email, name, password)
 VALUES ("vasilisa@yandex.ru", "Василиса", "rnjnfv"),
         ("nikifor@gmail.com", "Никифор", "zpltcm");
+
+/*
+ Добавление новых записей в таблицу projects
+*/
+
+INSERT INTO projects (user_id, name)
+VALUES (1, "Входящие"),
+       (1, "Учёба"),
+       (1, "Работа"),
+       (1, "Домашние дела"),
+       (1, "Авто"),
+       (1, "Ремонт"),
+       (2, "Главное"),
+       (2, "Проект"),
+       (2, "Учёба"),
+       (2, "Игра"),
+       (2, "Туризм");
