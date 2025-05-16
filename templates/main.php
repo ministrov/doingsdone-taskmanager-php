c6032929dde875cfff7eafb925a18f4026f562d6
Zhila <ministrov2018@gmail.com>
2025-05-16 10:51:43 +0300

    fix - Change function to safe_html in main template

--- templates/main.php
+++ templates/main.php
@@ -6,7 +6,7 @@
       <ul class="main-navigation__list">
         <?php foreach ($projects as $project): ?>
           <li class="main-navigation__list-item">
-            <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($project["name"]) ?></a>
+            <a class="main-navigation__list-item-link" href="#"><?= safe_html($project["name"]) ?></a>
             <span class="main-navigation__list-item-count">
               0
             </span>
